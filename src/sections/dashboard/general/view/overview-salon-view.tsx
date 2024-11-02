ab3b733c1d7883be1a0ea42c9f5fc785e901ec4f
afsal983 <82200003+afsal983@users.noreply.github.com>
2024-11-02 14:49:53 +0400

    update

--- src/sections/dashboard/general/view/overview-salon-view.tsx
+++ src/sections/dashboard/general/view/overview-salon-view.tsx
@@ -160,21 +160,6 @@ export default function OverviewSalonView() {
 
         <Grid xs={12} md={8}>
           <Stack spacing={3}>
-            <LatestTransactions
-              title="Latest Transactions"
-              tableData={latestsales.data}
-              tableLabels={[
-                { id: "orderid", label: "Order ID" },
-                { id: "billingname", label: "Billing Name" },
-                { id: "employeename", label: "Employee Name" },
-                { id: "date", label: "Date" },
-                { id: "total", label: "Total" },
-                { id: "paymentstatus", label: "Payment Status" },
-                { id: "paymentmethod", label: "Payment Method" },
-                { id: "" },
-              ]}
-            />
-
             <SalonYearlySales
               title="Yearly Sales"
               subheader="(+43%) than last year"
